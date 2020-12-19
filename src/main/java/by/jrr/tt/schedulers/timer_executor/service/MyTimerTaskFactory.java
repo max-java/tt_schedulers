8d3f9d7295887f2755b2a7ec4990a3068bfd8d54
max-java <6666350@gmail.com>
2020-12-19 23:59:16 +0300

    removes unused

--- src/main/java/by/jrr/tt/schedulers/timer_executor/service/MyTimerTaskFactory.java
+++ src/main/java/by/jrr/tt/schedulers/timer_executor/service/MyTimerTaskFactory.java
@@ -2,14 +2,9 @@ package by.jrr.tt.schedulers.timer_executor.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
-import java.util.Timer;
 import java.util.TimerTask;
 
-@Service
 public class MyTimerTaskFactory {
 
     final Logger logger = LoggerFactory.getLogger("[timer]");
